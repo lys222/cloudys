6ccf235b31f8e96e3e44383e75d907b0986de740
lys222 <youngseo222@gmail.com>
2025-03-24 16:20:48 +0900

    relocate hits position

--- src/routes/Feed/Footer.tsx
+++ src/routes/Feed/Footer.tsx
@@ -22,7 +22,7 @@ const Footer: React.FC<Props> = ({ className }) => {
           © {CONFIG.profile.name} {from === y || !from ? y : `${from} - ${y}`}
         </a>
         <HitCounter href="https://hits.seeyoufarm.com">
-          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter" />
+          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fcloudys.azurewebsites.net&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
         </HitCounter>
       </FooterContainer>
     </StyledWrapper>
@@ -43,12 +43,11 @@ const FooterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
+  flex-direction: column;
   width: 100%;
   padding: 16px;
 `;
 
 const HitCounter = styled.a`
-  position: absolute;
-  right: 16px;
+  margin-top: 8px;
 `;
