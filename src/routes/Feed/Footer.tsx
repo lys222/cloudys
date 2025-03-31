275a0423915f3a203cbfd2c9d855b63e50060e1b
lys222 <youngseo222@gmail.com>
2025-03-31 15:52:16 +0900

    exclude hits

--- src/routes/Feed/Footer.tsx
+++ src/routes/Feed/Footer.tsx
@@ -21,9 +21,9 @@ const Footer: React.FC<Props> = ({ className }) => {
         >
           © {CONFIG.profile.name} {from === y || !from ? y : `${from} - ${y}`}
         </a>
-        <HitCounter href="https://hits.seeyoufarm.com">
+        {/* <HitCounter href="https://hits.seeyoufarm.com">
           <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fcloudys.azurewebsites.net&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
-        </HitCounter>
+        </HitCounter> */}
       </FooterContainer>
     </StyledWrapper>
   )
