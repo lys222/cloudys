518efb04dc002c7bf21677e5b1e13041e6f99ed7
lys222 <youngseo222@gmail.com>
2025-03-31 16:28:44 +0900

    add progress bar

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,10 +1,12 @@
-import React, { ReactNode, useEffect } from "react"
+import React, { useState, useEffect, useRef, ReactNode } from "react"
 import { ThemeProvider } from "./ThemeProvider"
 import useScheme from "src/hooks/useScheme"
 import Header from "./Header"
 import styled from "@emotion/styled"
 import Scripts from "src/layouts/RootLayout/Scripts"
 import useGtagEffect from "./useGtagEffect"
+import useThrottle from "src/hooks/useThrottles"
+import { useRouter } from "next/router"
 import Prism from "prismjs/prism"
 import 'prismjs/components/prism-markup-templating.js'
 import 'prismjs/components/prism-markup.js'
@@ -45,19 +47,55 @@ type Props = {
 }
 
 const RootLayout = ({ children }: Props) => {
+  const router = useRouter()
+  const currentElementRef = useRef<HTMLDivElement | null>(null)
+
+  const [blogHeight, setBlogHeight] = useState(0)
+  const [throttleScrollY, setThrottleScrollY] = useState<number>(0)
   const [scheme] = useScheme()
   useGtagEffect()
+
+  const scrollThrottle = useThrottle(() => {
+    setThrottleScrollY(window.scrollY)
+  }, 100)
+
+  const getCurrentPercentage = () => {
+    if (router.asPath === "/") return 0
+
+    let percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
+
+    if (percentage >= 90) {
+      percentage = 100
+    } else {
+      percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
+    }
+    return percentage
+  }
+
   useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+    if (currentElementRef.current) {
+      const clientHeight = currentElementRef.current.clientHeight
+      setBlogHeight(clientHeight)
+    }
+
+    window.addEventListener("scroll", scrollThrottle)
+    return () => window.removeEventListener("scroll", scrollThrottle)
+  }, [throttleScrollY])
+
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
 
   return (
     <ThemeProvider scheme={scheme}>
       <Scripts />
       {/* // TODO: replace react query */}
       {/* {metaConfig.type !== "Paper" && <Header />} */}
-      <Header fullWidth={false} />
-      <StyledMain>{children}</StyledMain>
+      <Header
+        fullWidth={false}
+        readingProgress={blogHeight < 1200 ? 0 : getCurrentPercentage()}
+      />
+      <StyledMain ref={currentElementRef}>{children}</StyledMain>
     </ThemeProvider>
   )
 }
